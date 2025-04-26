523cdb457bcdabea146cf62828fcc6c6679a4638
Patrick72444 <patbran07@gmail.com>
2025-04-26 19:12:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request
-from kucoin.client import Client
+from kucoin_futures.client import Trade
 import requests
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
 # Cliente de KuCoin
-client = Client(api_key, api_secret, api_passphrase, sandbox=False)
+client = Trade(api_key=api_key, secret=api_secret, passphrase=api_passphrase, is_sandbox=False)
 
 # Configuraciones
 symbol = "BTCUSDCM"  # ✅ BTC/USDC perpetual en KuCoin Futures
